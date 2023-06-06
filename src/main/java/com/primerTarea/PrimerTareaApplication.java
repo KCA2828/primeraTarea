d9ef0cabd2b48ed1d2aa40822e7c9d77ae2ddae0
Kenneth <21kenneth85@gmail.com>
2023-06-05 19:59:51 -0600

    Se realizo la tarea de semana tres tratando de incluir los temas vistos en clase

---
+++ src/main/java/com/primerTarea/PrimerTareaApplication.java
@@ -0,0 +1,13 @@
+package com.primerTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerTareaApplication.class, args);
+	}
+
+}
